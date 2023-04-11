f44c7512233e4d9148e74c00c0db273e1615e0a0
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2023-04-11 20:27:51 +0530

    Hoop Jump(Code Chef-LP1TO201)
    Source Code of Hoop Jump question from LP1TO201 using C language.

--- HoopJump.c
+++ HoopJump.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-void ho(int h){
-    int p1=1;
-    int p2=h;
+void ho(int h){// A funtion is made
+    int p1=1;// Player1 starts from Hoop 1
+    int p2=h; // Player2 starts from the Last hoop named h(refernce variable)
     for(int i=0;i<(h/2)+1;i++){
         if(p1!=p2){
-            p1++;
-            p2--;
+            p1++;// Player1 goes forward every hoop by 1 
+            p2--;// Player2 goes backward every hoop by 2
         }
         else{
-            printf("%d",p1);
+            printf("%d",p1); // if no hoop is left for both players i.e. both land on same hoop then the condition fails and breaks
             break;
         }
     }
@@ -19,7 +19,7 @@ int main(){
     scanf("%d",&test);
     for(int i=0;i<test;i++){
         scanf("%d",&hoops);
-        ho(hoops);
+        ho(hoops);// The function is called again here
     }
     return 0;
 }
